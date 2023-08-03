86b895cb321b4580b6f42e05d05fc27845d261b2
Aditya Saini <Adityasaini10012001@outlook.com>
2023-08-03 19:35:34 +0530

    added starting files
    Signed-off-by: Aditya Saini <Adityasaini10012001@outlook.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ChartAndEmail from "./components/chartsandemail"
+import "./App.css"
 
 function App() {
+  const [file, setFile] = useState();
+  const [array, setArray] = useState([]);
+
+  const fileReader = new FileReader();
+
+  const handleOnChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const csvFileToArray = (string) => {
+    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+
+    const newArray = csvRows.map((i) => {
+      const values = i.split(",");
+      const obj = csvHeader.reduce((object, header, index) => {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return obj;
+    });
+
+    setArray((prevArray) => [...prevArray, ...newArray]);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (file) {
+      fileReader.onload = function (event) {
+        const text = event.target.result;
+        csvFileToArray(text);
+      };
+
+      fileReader.readAsText(file);
+    }
+  };
+
+  const headerKeys = Object.keys(array.length > 0 ? array[0] : {});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ textAlign: "center" }}>
+      <h1>REACTJS CSV</h1>
+      <form>
+        <input
+          type={"file"}
+          id={"csvFileInput"}
+          accept={".csv"}
+          onChange={handleOnChange}
+        />
+
+        <button
+          onClick={(e) => {
+            handleOnSubmit(e);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          IMPORT CSV
+        </button>
+      </form>
+
+      <br />
+
+      <table>
+        <thead>
+          <tr key={"header"}>
+            {headerKeys.map((key) => (
+              <th key={key}>{key}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {array.map((item, index) => (
+            <tr key={index}>
+              {Object.values(item).map((val, idx) => (
+                <td key={idx}>{val}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+  
     </div>
   );
 }
